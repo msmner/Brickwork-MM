2d5a7bfe573a249fae2f717f8a5355649835bcaf
msmner <ivaylo.nikolov88@gmail.com>
2021-01-22 19:57:59 +0200

    create project

---
+++ Brickwork/Brickwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brickwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
